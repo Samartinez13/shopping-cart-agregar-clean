6306b51e4b66682d4d2d74e1dc4a94c734f3c976
Samuel Martinez <sa.da.martinez@unillanos.edu.co>
2026-04-23 09:52:41 -0500

    Create ConfirmarCarritoUseCase.java

---
+++ src/main/java/isi/shoppingCart/usecases/services/ConfirmarCarritoUseCase.java
@@ -0,0 +1,16 @@
+package isi.shoppingCart.usecases.services;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ConfirmarCarritoUseCase extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
